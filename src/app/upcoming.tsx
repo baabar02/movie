649f25ed061d0a5b4744138b6e2fbd993b113d1a
baabar02 <baabarmx@gmail.com>
2025-05-08 20:59:11 +0800

    s

--- src/app/upcoming.tsx
+++ src/app/upcoming.tsx
@@ -17,7 +17,7 @@ export const Upcoming = () => {
               <div className="p-1">
                 <Card>
                   <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-4xl font-semibold">{index + 1}</span>
+                    <span className="text-4xl font-semibold">{index + 1} text</span>
                   </CardContent>
                 </Card>
               </div>
